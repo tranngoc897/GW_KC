c9eb1cc216fa1073de5adcecffff580fcf008403
Ngoc Tran <tranngoc897@gmail.com>
2022-03-08 17:33:20 +0700

    Initial commit

---
+++ RO/src/main/java/com/example/ro/RoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoApplication.class, args);
+    }
+
+}
---
+++ RO/src/test/java/com/example/ro/RoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
